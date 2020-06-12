2d59034cad6fe8e77ea71bf5d16968f7781703f4
Jyf010205 <jianyufeng@agree.com.cn>
2020-06-12 17:41:31 +0800

    第三章结束

---
+++ febs-gateway/src/main/java/com/sgzs/febs/gateway/filter/FebsGatewaySentinelFilter.java
@@ -0,0 +1,80 @@
+package com.sgzs.febs.gateway.filter;
+
+import com.alibaba.csp.sentinel.adapter.gateway.common.SentinelGatewayConstants;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiDefinition;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPathPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.ApiPredicateItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.api.GatewayApiDefinitionManager;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayFlowRule;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayParamFlowItem;
+import com.alibaba.csp.sentinel.adapter.gateway.common.rule.GatewayRuleManager;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.ZuulBlockFallbackManager;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulErrorFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPostFilter;
+import com.alibaba.csp.sentinel.adapter.gateway.zuul.filters.SentinelZuulPreFilter;
+import com.netflix.zuul.ZuulFilter;
+import com.sgzs.febs.gateway.fallback.FebsGatewayBlockFallbackProvider;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author: jianyufeng
+ * @description:
+ * @date: 2020/6/12 17:17
+ */
+@Slf4j
+@Configuration
+public class FebsGatewaySentinelFilter {
+    @Bean
+    public ZuulFilter sentinelZuulPreFilter(){
+        return new SentinelZuulPreFilter();
+    }
+
+    @Bean
+    public ZuulFilter sentinelZuulPostFilter(){
+        return new SentinelZuulPostFilter();
+    }
+
+    @Bean
+    public ZuulFilter sentinelZuulErrorFilter(){
+        return new SentinelZuulErrorFilter();
+    }
+
+    @PostConstruct
+    public void doInit(){
+        ZuulBlockFallbackManager.registerProvider(new FebsGatewayBlockFallbackProvider());
+        initGatewayRules();
+    }
+
+    private void initGatewayRules() {
+        Set<ApiDefinition> definitions = new HashSet<>();
+        Set<ApiPredicateItem> predicateItems = new HashSet<>();
+
+        predicateItems.add(new ApiPathPredicateItem().setPattern("/auth/captcha"));
+        ApiDefinition definition = new ApiDefinition("captcha")
+                .setPredicateItems(predicateItems);
+        definitions.add(definition);
+        GatewayApiDefinitionManager.loadApiDefinitions(definitions);
+
+        Set<GatewayFlowRule> rules = new HashSet<>();
+
+        rules.add(new GatewayFlowRule("captcha")
+                .setResourceMode(SentinelGatewayConstants.RESOURCE_MODE_CUSTOM_API_NAME)
+                .setParamItem(
+                        new GatewayParamFlowItem()
+                                .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_URL_PARAM)
+                                .setFieldName("key")
+                                .setMatchStrategy(SentinelGatewayConstants.PARAM_MATCH_STRATEGY_EXACT)
+                                .setParseStrategy(SentinelGatewayConstants.PARAM_PARSE_STRATEGY_CLIENT_IP)
+                )
+                .setCount(10)
+                .setIntervalSec(60)
+        );
+        GatewayRuleManager.loadRules(rules);
+    }
+}
